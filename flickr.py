d3c7b497381c394df835a68b2c3868e38f996754
Zelphir Kaltstahl <zelphirkaltstahl@gmail.com>
2018-02-07 00:07:40 +0100

    removed old commented out code and catching keyboard interrupt

--- flickr.py
+++ flickr.py
@@ -6,6 +6,7 @@ import random
 # import re
 import os
 import argparse
+import sys
 
 
 ##############
@@ -62,49 +63,10 @@ def sleep_random_time(min_time=1, max_time=5):
     time.sleep(duration)
 
 
-# def download_image_file(photoset_id, photo_id, image_url):
-#     img = urllib.request.urlopen(image_url)
-#     with open(f"{photoset_id}/{photo_id}.jpg", "wb") as image_file:
-#         image_file.write(img.read())
-
-
 def get_attribute_from_photo(photo, attribute):
     for photo_item in photo.items():
         if photo_item[0] == attribute:
             return photo_item[1]
-
-
-# def build_photo_overview_page_url(user_id, photo_id):
-#     """builds the URL to the overview page for the photo id"""
-#     return f"https://www.flickr.com/photos/{user_id}/{photo_id}/sizes/o/"
-#     # return f"https://www.flickr.com/photos/{user_id}/{photo_id}"
-
-
-# def get_original_image_page_url(tree):
-#     # <a href="/photos/159290953@N04/39185270745/sizes/o/" id="yui_3_11_0_3_1517773310172_368">
-#     # Original</a>
-#     # //a[text()='text_i_want_to_find']/@href
-#     result = tree.xpath("//div[@id='all-sizes-header']//a[text()='Original']/@href")
-#     print(result)
-#     return result
-#     # find link to original image page
-#     # return it
-#     # pass
-
-
-# def get_image_src(tree):
-#     try:
-#         image_src = tree.xpath("//div[@id='all-sizes-header']//"
-#                                "a[contains(text(), 'Download the Original size of this photo')]/"
-#                                "@href")[0]
-#         print(image_src)
-#         return image_src
-#     except IndexError as exc:
-#         print("result of xpath without index:")
-#         print(tree.xpath("//div[@id='all-sizes-header']//"
-#                          "a[contains(text(), 'Download the Original size of this photo')]/"
-#                          "@href"))
-#         raise exc
 
 
 def create_directory_if_not_exists(dir_path):
@@ -118,36 +80,6 @@ def build_dir_path(photoset_id, photo_id, prefix="downloaded-photos"):
 
 def build_file_path(photoset_id, photo_id, image_src, prefix="downloaded-photos"):
     return os.path.join(prefix, photoset_id, photo_id) + image_src[-4:]
-
-
-# def download_photo(user_id, photoset_id, photo_id):
-#     print(f"downloading photo with id: {photo_id}")
-#     parser = etree.HTMLParser()
-
-#     # image overview page
-#     image_overview_page_url = build_photo_overview_page_url(user_id, photo_id)
-#     print("image overview page url:", image_overview_page_url)
-#     # overview_page_response = urllib.request.urlopen(image_overview_page_url)
-#     # overview_page_tree = etree.parse(overview_page_response, parser)
-
-#     # original image page
-#     # original_image_url = get_image_src(overview_page_tree)
-#     # print("original image url:", original_image_url)
-
-#     with urllib.request.urlopen(image_overview_page_url) as stream:
-#         tree = etree.parse(stream, parser)
-#         # print(tree)
-#         image_src = get_image_src(tree)
-#         print(f"downloading image from: {image_src}")
-
-#         dir_path = build_dir_path(user_id, photoset_id, photo_id)
-#         file_path = build_file_path(user_id, photoset_id, photo_id, image_src)
-#         create_directory_if_not_exists(dir_path)
-
-#         with open(file_path, mode="wb") as image_file:
-#             image_file.write(urllib.request.urlopen(image_src).read())
-#         print("image file written to disk at: {}".format(
-#             os.path.join(user_id, photoset_id, photo_id)))
 
 
 def download_image_from_url(
@@ -176,9 +108,9 @@ def get_photoset_infos(all_sets):
             "name": photoset.find("title").text,
             "photos": set()
         }
-        print(photosets)
+        # print(photosets)
         for photo in flickr.walk_set(photoset_id):
-            print(photo.items())
+            # print(photo.items())
             photosets[photoset_id]["photos"].add(
                 (get_attribute_from_photo(photo, "id"),
                  get_attribute_from_photo(photo, "title")))
@@ -186,6 +118,11 @@ def get_photoset_infos(all_sets):
 
 
 def download_photos(flickr, photosets):
+    num_of_photos = sum([len(photoset["photos"])
+                         for (photoset_id, photoset)
+                         in photosets.items()])
+    print(f"[INFO]: found {num_of_photos} photos.")
+
     for (photoset_id, photoset) in photosets.items():
         for (photo_id, photo_name) in photoset["photos"]:
             sizes_tree = flickr.photos.getSizes(photo_id=photo_id)
@@ -200,17 +137,19 @@ def download_photos(flickr, photosets):
 
 
 if __name__ == "__main__":
-    user_id = args.user_id
-    api_key = args.api_key
-    api_secret = args.api_secret
+    try:
+        user_id = args.user_id
+        api_key = args.api_key
+        api_secret = args.api_secret
 
-    flickr = flickrapi.FlickrAPI(api_key, api_secret, format="etree")
-    flickr.authenticate_via_browser(perms='read')
-    sets = flickr.photosets.getList(user_id=user_id)
-    all_sets = sets.find('photosets').findall('photoset')
+        flickr = flickrapi.FlickrAPI(api_key, api_secret, format="etree")
+        flickr.authenticate_via_browser(perms="read")
+        sets = flickr.photosets.getList(user_id=user_id)
+        all_sets = sets.find("photosets").findall("photoset")
 
-    photosets = get_photoset_infos(all_sets)
+        photosets = get_photoset_infos(all_sets)
 
-    # print("got the following photosets:", photosets)
-
-    download_photos(flickr, photosets)
+        download_photos(flickr, photosets)
+    except KeyboardInterrupt as exc:
+        print("[INFO]: got keyboard interrupt, exiting ...")
+        sys.exit(0)
